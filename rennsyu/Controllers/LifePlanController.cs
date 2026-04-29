5eaf7910f87f1a59fd7179c92a4d226225efd4a6
Kinagaki-525 <kensuke5251999@gmail.com>
2026-04-29 19:46:20 +0900

    マスク設定を追加

--- rennsyu/Controllers/LifePlanController.cs
+++ rennsyu/Controllers/LifePlanController.cs
@@ -13,11 +13,30 @@ namespace rennsyu.Controllers
             this.lifePlanPageService = lifePlanPageService;
         }
 
+        // TODO: 機能実装完了後に削除 - 公開時はTOPページにリダイレクト
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        /*
+        // TODO: 機能実装完了後に有効化
         public IActionResult Index()
         {
             return View(lifePlanPageService.CreateInitialPage());
         }
+        */
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // TODO: 機能実装完了後に削除 - 公開時はTOPページにリダイレクト
+        public IActionResult Index(LifePlanViewModel model)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        /*
+        // TODO: 機能実装完了後に有効化
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Index(LifePlanViewModel model)
@@ -31,5 +50,6 @@ namespace rennsyu.Controllers
 
             return View(result.Page);
         }
+        */
     }
 }
